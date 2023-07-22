80574ed59041314dc849b228934e39c6a4ff721a
Abhishek Gupta <91587690+iamabhigupta@users.noreply.github.com>
2023-07-22 18:57:32 +0530

    Sidebar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,12 +1,10 @@
 import { Button } from '@/components/ui/button';
-import { UserButton } from '@clerk/nextjs';
 
 const DashboardPage = () => {
   return (
     <>
       <p className="text-xl text-red-500">Abhishek (Protected)</p>
       <Button variant="secondary">Abhishek</Button>
-      <UserButton afterSignOutUrl="/" />
     </>
   );
 };
